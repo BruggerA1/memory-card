6b118d6cd006bfd7fffbfb9c2b665da7ac77f741
Brugger Anderson <BruggerA1@gmail.com>
2022-05-29 14:58:37 -0400

    main branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
-import sprites from "./utilities/importSprites";
 
 const App = () => {
   return (
     <div className="App">
-      {sprites.map(sprite => <img src={sprite}/>)}
     </div>
   );
 };
